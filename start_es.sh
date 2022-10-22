195fc4389f3b4420d6a373ae26b76750224e7e74
Clément CHAZALET <clementchazalet@gmail.com>
2022-10-22 12:31:04 -0400

    A valider

--- start_es.sh
+++ start_es.sh
@@ -5,10 +5,11 @@ read -p 'Username: ' uservar
 read -sp 'Password: ' passvar
 echo
 
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13
+for i in 01 02 03 
 do 
     echo "########### tc405-112-$i.insa-lyon.fr ###########"
-    sshpass -p "$passvar" ssh -o StrictHostKeyChecking=no $uservar@tc405-112-$i.insa-lyon.fr '/tmp/elasticsearch-7.10.2/bin/elasticsearch &'
+    sshpass -p "$passvar" ssh -o StrictHostKeyChecking=no $uservar@tc405-112-$i.insa-lyon.fr 'pkill -f elasticsearch'
+    sshpass -p "$passvar" ssh -o StrictHostKeyChecking=no $uservar@tc405-112-$i.insa-lyon.fr 'nohup /tmp/elasticsearch-7.10.2/bin/elasticsearch > /tmp/elastic.out 2> /tmp/elastic.err < /dev/null &'
     echo "tc405-112-$i.insa-lyon.fr : send command ok..."
 done
 
